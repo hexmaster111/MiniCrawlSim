27b274a66f724bd9a636e86cbf03c2c782266e8f
Hailey <Hexmaster111@gmail.com>
2022-11-04 07:13:59 -0500

    Initial commit

---
+++ MiniCrawlSim/MiniCrawlSim.h
@@ -0,0 +1,8 @@
+﻿// MiniCrawlSim.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
